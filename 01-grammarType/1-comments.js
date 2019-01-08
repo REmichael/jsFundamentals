9610c6d7da2548c837cc12d0d67fd245b4cd2039
michael reardon <michael.hdi@gmail.com>
2019-01-08 10:48:15 -0500

    first commit

---
+++ 01-grammarType/1-comments.js
@@ -0,0 +1,14 @@
+//A comment!
+//what comments do for us:
+//1) communicate with other developers by writing in plain English
+//2) annotate certain sectioins of our code for our future selves
+
+/*
+I can
+Comment out
+multiple lines!
+have a great day!
+*/
+
+console.log('yay');
+// console.log('Michael Reardon');
